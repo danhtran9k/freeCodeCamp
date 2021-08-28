d79b4b3132ba65739444b17fe21e9d6170f479f5
danh tran <tldanh1993@gmail.com>
2021-08-28 17:45:32 +0700

    basic Data Strcutures

---
+++ jsAlgorithmsAndData/js4_basicDataStruct_20/bs09_-SpreadArrCombine.js
@@ -0,0 +1,45 @@
+/*
+
+==================================================================
+LINK
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-data-structures/combine-arrays-with-the-spread-operator
+
+==================================================================
+DESCRIPTION
+
+Combine Arrays with the Spread Operator
+Another huge advantage of the spread operator is the ability to combine arrays, or to insert all the elements of one array into another, at any index. With more traditional syntaxes, we can concatenate arrays, but this only allows us to combine arrays at the end of one, and at the start of another. Spread syntax makes the following operation extremely simple:
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+thatArray would have the value ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander'].
+
+Using spread syntax, we have just achieved an operation that would have been more complex and more verbose had we used traditional methods.
+
+We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
+
+==================================================================
+TESTCASE
+
+spreadOut should return ["learning", "to", "code", "is", "fun"]
+
+The spreadOut function should utilize spread syntax
+
+==================================================================
+SETUP
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
+
+==================================================================
+
+*/
+
+
