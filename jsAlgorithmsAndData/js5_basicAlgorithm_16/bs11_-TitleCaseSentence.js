08067d0752d34c2f2401faf1ec2a1cef2f3b4a7f
danh tran <tldanh1993@gmail.com>
2021-08-28 17:56:49 +0700

    basic js algorithm

---
+++ jsAlgorithmsAndData/js5_basicAlgorithm_16/bs11_-TitleCaseSentence.js
@@ -0,0 +1,40 @@
+/*
+
+==================================================================
+LINK
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/title-case-a-sentence
+
+==================================================================
+DESCRIPTION
+
+Title Case a Sentence
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+==================================================================
+TESTCASE
+
+titleCase("I'm a little tea pot") should return a string.
+
+titleCase("I'm a little tea pot") should return the string I'm A Little Tea Pot.
+
+titleCase("sHoRt AnD sToUt") should return the string Short And Stout.
+
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return the string Here Is My Handle Here Is My Spout.
+
+==================================================================
+SETUP
+
+function titleCase(str) {
+  return str;
+}
+
+titleCase("I'm a little tea pot");
+
+==================================================================
+
+*/
+
+
---
+++ jsAlgorithmsAndData/js5_basicAlgorithm_16/bs12_-SliceSplice.js
@@ -0,0 +1,48 @@
+/*
+
+==================================================================
+LINK
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/slice-and-splice
+
+==================================================================
+DESCRIPTION
+
+Slice and Splice
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+
+==================================================================
+TESTCASE
+
+frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+
+frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
+
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+
+All elements from the first array should be added to the second array in their original order.
+
+The first array should remain the same after the function runs.
+
+The second array should remain the same after the function runs.
+
+==================================================================
+SETUP
+
+function frankenSplice(arr1, arr2, n) {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+==================================================================
+
+*/
+
+
