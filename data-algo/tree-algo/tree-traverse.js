5ac1952bbc7892e1cf37079e983741db5698f454
Danh Tran <danh2k@nexondv.com>
2024-10-14 13:54:20 +0700

    tmp-serialize

--- data-algo/tree-algo/tree-traverse.js
+++ data-algo/tree-algo/tree-traverse.js
@@ -37,3 +37,41 @@ export function travel_order(node) {
   }
   return result
 }
+
+export const serializeBFSTree = (head) => {
+  const result = []
+  if (!head) return null
+  const queue = [head]
+
+  let hasNextLevel = true
+  let nullCount = 0
+
+  while (queue.length > 0 && hasNextLevel) {
+    let maxLen = queue.length
+    hasNextLevel = false
+
+    for (let i = 0; i < maxLen; i++) {
+      const curr = queue.shift()
+      result.push(curr && curr.data)
+
+      if (!curr) continue
+      if (curr.left || curr.right) hasNextLevel = true
+
+      if (curr.left !== null) {
+        queue.push(...Array(nullCount).fill(null), curr.left)
+        nullCount = 0
+      } else {
+        nullCount++
+      }
+
+      if (curr.right !== null) {
+        queue.push(...Array(nullCount).fill(null), curr.right)
+        nullCount = 0
+      } else {
+        nullCount++
+      }
+    }
+    console.log('after: ', { result })
+  }
+  return result
+}
