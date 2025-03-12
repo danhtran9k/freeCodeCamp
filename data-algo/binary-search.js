a29354f21e1c2d0d770bb0b47000544828404a30
Tran Luong Danh [danh2k] <danh2k@nexondv.com>
2025-03-12 18:03:52 +0700

    setup nodemon - debugger

---
+++ data-algo/binary-search.js
@@ -0,0 +1,59 @@
+// 4, 6
+// non-descreasing order
+const reset = (arr) => [0, arr.length - 1]
+
+const find_First_GreatestNegative_Index = (arr) => {}
+const find_Last_GreatestNegative_Index = (arr) => {
+  let [left, right] = reset(arr)
+
+  const MODE = 'include'
+  const cond = {
+    exclude: (index, arr, left = 0, right = 0) => {
+      return arr[index] >= 0
+    },
+    include: (index, arr, left = 0, right = 0) => {
+      return arr[index] < 0
+    }
+  }
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2)
+
+    switch (MODE) {
+      case 'exclude':
+        if (cond[MODE](mid, arr)) {
+          right = mid
+        } else {
+          left = mid + 1
+        }
+        break
+      case 'include':
+        if (cond[MODE](mid, arr)) {
+          left = mid
+        } else {
+          right = mid - 1
+        }
+        break
+      default:
+        return null
+    }
+  }
+
+  return left - 1
+}
+
+// 7, 9
+const find_first_MinNonPositive_Index = (arr) => {}
+const find_last_MinNonPositive_Index = (arr) => {}
+
+// 10, 12
+const find_first_MinPositive_Index = (arr) => {}
+const find_last_MinPositive_Index = (arr) => {}
+
+// 4-6, 7-9, 10-12
+const test = [-4, -4, -4, -3, -2, -2, -2, 0, 0, 0, 2, 2, 2, 3, 4, 4, 4]
+// 10-12, 7-9, 4-6
+// const test = [4, , 4, 4 , 3, 2, 2, 2, 0, 0, 0, -2, -2, -2, -3, -4,-4,-4]
+export const main = () => {
+  return find_Last_GreatestNegative_Index(test)
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+// quicklog for leetcode
+import * as main from './data-algo/binary-search.js'
+
+const log = (val) => console.log(val)
+
+log(main.main())
