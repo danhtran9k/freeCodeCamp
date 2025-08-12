44115d2a2106a0b161bd550b464e12bcdbbb696e
Danh-m1 <danh2k@nexondv.com>
2025-08-12 20:55:21 +0700

    fix binary search simple

--- data-algo/binary-template-simple.js
+++ data-algo/binary-template-simple.js
@@ -1,5 +1,7 @@
+import fs from 'fs'
+
 const arr = [3, 3, 3, 7, 7, 10, 10, 10, 12, 15, 15, 15, 15]
-//           0, 1, 2, 3, 4,  5,  6,  7,  8,  9, 10, 11
+//           0, 1, 2, 3, 4,  5,  6,  7,  8,  9, 10, 11, 12
 const test = [2, 3, 10, 12, 15, 20]
 // expect is [lower, upper] of test in relative to arr
 const expect = [
@@ -11,72 +13,103 @@ const expect = [
   [11, -1]
 ]
 
+// write an function save results to file
+const saveResultsToFile = (results) => {
+  fs.writeFileSync('results.dump.json', JSON.stringify(results, null, 2))
+}
+
 /*
 -> target = 10 
+const arr = [3,...7, 10, 10, 10, 12, ...15]
+const idx = [0,...4,  5,  6,  7,  8, ...12]
 
-# Case 1
-max index num < target = 4
+(target < max ix num  = lastIx !! -> ĐN sai)
+1. UPPER BOUND ( TARGET < | <= MIN ix_num) 
+1.1 EXCLUDE T < N -> ix = 8
+1.2 INCLUDE T <= N -> ix = 7
 
-# Case 2
-max index num <= target = 7
+target = 2 -> index = 0
+target = 16 -> index = 13
 
-# Case 3
-min index num > target = 8
-target = 16 -> index = 12
-target = 2 -> index = -1
-
-# Case 4
-min index num >= target = 5
-
+2. LOWER BOUND ( MAX ix_num < | <= TARGET )
+2.1 EXCLUDE T > N -> ix = 4
+2.2 INCLUDE T >= N -> ix = 5
 */
 
-//
-/*
-CASE 3 : min index num > target
-Target = 10 -> index = 8
-target = 16 -> index = 12
-target = 2 -> index = -1
-*/
-const min_index_greater_than_target = (arr, target) => {
-  let [left, right] = [0, arr.length]
+const init = () => [0, arr.length]
+const midIx = (left, right) => Math.floor(left + (right - left) / 2)
+
+// CASE 1.1 UPPER BOUND EXCLUDE (max Index num < target)
+const upper_exclude = (arr, target) => {
+  let [left, right] = init()
   while (left < right) {
-    const mid = Math.floor(left + (right - left) / 2)
+    const mid = midIx(left, right)
     const cond = arr[mid] > target
     if (cond) {
+      // target < arr[mid]
       right = mid
     } else {
+      // arr[mid] <= target
       left = mid + 1
     }
   }
   return left
 }
 
-const max_index_less_than_target = (arr, target) => {
-  let [left, right] = [0, arr.length]
-  console.log({ target })
+// CASE 1.2 UPPER BOUND INCLUDE (max Index num <= target)
+const upper_include = (arr, target) => {
+  let [left, right] = init()
+  // console.log({ target })
   while (left < right) {
-    const mid = Math.floor(left + (right - left) / 2)
+    const mid = midIx(left, right)
     const cond = arr[mid] >= target
-    console.log({ cond, mid, left, right, nMid: arr[mid] })
+
     if (cond) {
+      // target <= arr[mid]
       right = mid
     } else {
+      // arr[mid] < target
       left = mid + 1
     }
   }
 
   return left
 }
+
+// CASE 2.1 LOWER BOUND EXCLUDE (Max idx 4 < TARGET = 10 )
+// CASE 2.2 LOWER BOUND INCLUDE (Max idx 5 <= TARGET = 10 )
+const lower_exclude = (arr, target) => upper_include(arr, target) - 1
+const lower_include = (arr, target) => upper_exclude(arr, target) - 1
 
 const debug = () => {
-  // const targets = [1, 3, 10, 12, 15, 20]
-  const targets = [10]
+  const targets = [1, 3, 4, 10, 11, 12, 15, 20]
+  // const targets = [10]
+  const results = []
+
   for (const target of targets) {
-    // const minIndexGreaterThanTarget = min_index_greater_than_target(arr, target)
-    // console.log({ target, minIndexGreaterThanTarget })
-    const maxIndexLessThanTarget = max_index_less_than_target(arr, target)
-    console.log({ target, maxIndexLessThanTarget })
+    const upperExclude = upper_exclude(arr, target)
+    const upperInclude = upper_include(arr, target)
+    const lowerExclude = lower_exclude(arr, target)
+    const lowerInclude = lower_include(arr, target)
+    const resTarget = {
+      target,
+      upperExclude,
+      upperInclude,
+      lowerExclude,
+      lowerInclude
+    }
+    results.push(resTarget)
+    console.log(resTarget)
+    console.log('========================================')
   }
+
+  saveResultsToFile(results)
 }
 
 debug()
+
+/*
+Case max Index lower Bound 
+Biên max + 1 khi -1 sẽ về lastIndex thực
+Biên -1 do 0 - 1
+*/
