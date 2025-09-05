be685dc231968e44f5c6eb088fdf599a360f1530
Danh-m1 <danh2k@nexondv.com>
2025-09-05 08:13:00 +0700

    tarjan

--- data-algo/graph-algo/tarjan_articulation.js
+++ data-algo/graph-algo/tarjan_articulation.js
@@ -187,6 +187,14 @@ export const tc_articulation = {
       [2, 7],
       [6, 2]
     ]
+  },
+  tcBasic: {
+    V: 5,
+    adjs: [[2, 3], [0], [1], [4]]
+  },
+  tcBasicUndireced: {
+    V: 5,
+    adjs: [[1, 2, 3], [0, 2], [0, 1], [0, 4], [3]]
   }
 }
 
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { main_articulation_run } from './data-algo/graph-algo/tarjan_articulation'
+import { main_scc_run } from './data-algo/graph-algo/tarjan_scc'
 
 function minMalwareSpread(graph: number[][], initial: number[]): number {
   const len = graph.length
@@ -49,6 +49,76 @@ function minMalwareSpread(graph: number[][], initial: number[]): number {
   return minIndex
 }
 
+function minMalwareSpread2(graph: number[][], initial: number[]): number {
+  const { dfs, countSub, visited } = tarjan_infect_setup(graph, initial)
+
+  let maxSize = 0
+  let minIndex = Infinity
+
+  for (const node of initial) {
+    if (visited[node] === -1) {
+      dfs(node, node)
+    }
+
+    const size = countSub[node]
+    if (size > maxSize || (size === maxSize && node < minIndex)) {
+      maxSize = size
+      minIndex = node
+    }
+  }
+
+  return minIndex
+}
+
+const tarjan_infect_setup = (graph: number[][], initial: number[]) => {
+  const len = graph.length
+
+  let id = 0
+  const ids = Array(len).fill(-1)
+  const visited = Array(len).fill(-1)
+
+  const initialSet = new Set(initial)
+  const countSub = Array(len).fill(0)
+
+  // const parentSize: Record<number, number> = {}
+  // vì đi tiếp khi gặp source khác, ko skip nên phải là array
+  // ngoài ra dùng array khỏi track parent color -> vốn fail bài này
+
+  const dfs = (node, rootStart) => {
+    ids[node] = id
+    visited[node] = id
+    id++
+
+    let size = 1
+    let flag = initialSet.has(node) && node !== rootStart
+    // có flag nhưng vẫn duyệt đủ, ko skip như 1
+
+    const neighbougrs = graph[node]
+    for (let neighb = 0; neighb < len; neighb++) {
+      if (!neighbougrs[neighb]) continue
+
+      if (ids[neighb] === -1) {
+        const childSize = dfs(neighb, -1)
+        if (!childSize) flag = true
+        size += childSize // + 0 ko đổi
+
+        if (visited[neighb] >= ids[node]) {
+          countSub[node] += childSize
+          // add khi node là articulation point
+        }
+
+        visited[node] = Math.min(visited[node], visited[neighb])
+      } else {
+        visited[node] = Math.min(visited[node], ids[neighb])
+      }
+    }
+
+    return flag ? 0 : size
+  }
+
+  return { dfs, visited, countSub }
+}
+
 const graph = [
   [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -63,6 +133,14 @@ const graph = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 ]
 const initial = [7, 8, 6, 2, 3]
-console.log(minMalwareSpread(graph, initial))
 
-main_articulation_run()
+const graph2 = [
+  [1, 1, 0],
+  [1, 1, 0],
+  [0, 0, 1]
+]
+const initial2 = [0, 1]
+
+// main_scc_run()
+const result2 = minMalwareSpread2(graph2, initial2)
+console.log(result2)
