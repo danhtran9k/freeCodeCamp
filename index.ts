ca50b84536ba8c9b99f41518e2e9c14285576dd2
Danh-m1 <danh2k@nexondv.com>
2025-09-10 07:57:20 +0700

    tmp-daily

--- index.ts
+++ index.ts
@@ -74,4 +74,121 @@ const lc = () => {
     const res = findCriticalAndPseudoCriticalEdges(n, edges)
     console.log(res)
 }
-lc()
+// lc()
+
+const tc_daily = {
+    1: {
+        n: 2,
+        languages: [[1], [2], [1, 2]],
+        friendships: [
+            [1, 2],
+            [1, 3],
+            [2, 3]
+        ]
+    },
+    2: {
+        n: 3,
+        languages: [[2], [1], [1, 2], [3]],
+        friendships: [
+            [1, 4],
+            [1, 2],
+            [3, 4],
+            [2, 3]
+        ]
+    },
+    5: {
+        n: 11,
+        languages: [
+            [3, 11, 5, 10, 1, 4, 9, 7, 2, 8, 6],
+            [5, 10, 6, 4, 8, 7],
+            [6, 11, 7, 9],
+            [11, 10, 4],
+            [6, 2, 8, 4, 3],
+            [9, 2, 8, 4, 6, 1, 5, 7, 3, 10],
+            [7, 5, 11, 1, 3, 4],
+            [3, 4, 11, 10, 6, 2, 1, 7, 5, 8, 9],
+            [8, 6, 10, 2, 3, 1, 11, 5],
+            [5, 11, 6, 4, 2]
+        ],
+        friendships: [
+            [7, 9],
+            [3, 7],
+            [3, 4],
+            [2, 9],
+            [1, 8],
+            [5, 9],
+            [8, 9],
+            [6, 9],
+            [3, 5],
+            [4, 5],
+            [4, 9],
+            [3, 6],
+            [1, 7],
+            [1, 3],
+            [2, 8],
+            [2, 6],
+            [5, 7],
+            [4, 6],
+            [5, 8],
+            [5, 6],
+            [2, 7],
+            [4, 8],
+            [3, 8],
+            [6, 8],
+            [2, 5],
+            [1, 4],
+            [1, 9],
+            [1, 6],
+            [6, 7]
+        ]
+    }
+}
+function minimumTeachings(
+    n: number,
+    languages: number[][],
+    friendships: number[][]
+): number {
+    let res = 0
+    const { langCount, langCountSorted, totalFriends, friendAdjs, totalLang } =
+        setup(n, languages, friendships)
+    let langChoose = langCountSorted[0]
+    return totalFriends - langCount[langChoose].length
+}
+
+const setup = (n, languages, friendships) => {
+    const totalPeople = languages.length
+    const langCount = {}
+    let totalLang = 0
+
+    for (let friend = 1; friend <= totalPeople; friend++) {
+        const ix = friend - 1
+        const knowns = languages[ix]
+        for (const lang of knowns) {
+            if (langCount[lang] === undefined) {
+                langCount[lang] = []
+                totalLang++
+            }
+            langCount[lang].push(friend)
+        }
+    }
+    const langCountSorted = Object.keys(langCount).sort(
+        (a, b) => langCount[b].length - langCount[a].length
+    )
+
+    const friendAdjs = {}
+    for (const [from, to] of friendships) {
+        if (friendAdjs[from] === undefined) friendAdjs[from] = []
+        if (friendAdjs[to] === undefined) friendAdjs[to] = []
+        friendAdjs[from].push(to)
+        friendAdjs[to].push(from)
+    }
+    const totalFriends = Object.keys(friendAdjs).length
+    return { langCount, langCountSorted, totalFriends, friendAdjs, totalLang }
+}
+
+const daily = () => {
+    const { n, languages, friendships } = tc_daily[5]
+    const res = minimumTeachings(n, languages, friendships)
+    console.log(res)
+}
+daily()
